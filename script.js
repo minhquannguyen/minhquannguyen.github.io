cff7d96ff5d1aacad8336e0b040b84ebeb097482
Minh <minhquanphungnguyen@yahoo.com>
2014-06-04 18:05:32 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+	var count = 0;
+	function myFunc(){
+		count++;
+		if(count %2 == 0){
+			document.getElementById("test").innerHTML = "even";
+		}
+		else{
+			document.getElementById("test").innerHTML = "odd";
+		}
+	}
+
+	// setInterval(function(){
+	// 	if(count == 0){
+	// 		document.getElementById("image").style.background = "url(pics/pic2.jpg)";
+	// 		document.getElementById("image").style.backgroundSize= "cover";
+	// 	}else if( count == 1){
+	// 		document.getElementById("image").style.background = "url(pics/pic1.jpg)";
+	// 		document.getElementById("image").style.backgroundSize = "cover";
+	// 	}else{
+	// 		document.getElementById("image").style.background = "url(pics/pic3.jpg)";
+	// 		count = 0;
+	// 	}
+	// 	console.log(count);
+	// 	count++;
+	// },3000);
+
+function mOver(obj){
+	obj.innerHTML = "minhquannguyen@berkeley.edu";
+}
+
+function mOut(obj){
+	obj.innerHTML = "Contact";
+}
